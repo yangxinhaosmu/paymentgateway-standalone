9f3618f7ac29703a9af92b99eef6d2d441270265
Matt Thornfield <matt@oyvey.org.uk>
2022-06-07 13:37:13 +0100

    Update getpaymentgateway.bat

--- getpaymentgateway.bat
+++ getpaymentgateway.bat
@@ -1,8 +1,12 @@
+echo off
 echo *** this script will download and run the payment gateway application. Always run this script from the command line ***
 pause
 if not exist paymentgateway-standalone\ (
-git clone https://github.com/vppmatt/paymentgateway-standalone.git
-)
+call git clone https://github.com/vppmatt/paymentgateway-standalone.git
 cd paymentgateway-standalone
-mvnw clean package
-java -jar target/java -jar target/payments-0.0.1-SNAPSHOT.jar
+) else (
+cd paymentgateway-standalone
+call git pull
+)
+call mvnw clean package
+java -jar target/payments-0.0.1-SNAPSHOT.jar
