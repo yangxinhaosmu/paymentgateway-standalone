5bd657adeb20f166d678a334f8f73df5bbfdf03b
Matt Thornfield <matt@oyvey.org.uk>
2023-06-07 10:56:36 +0100

    Update SwaggerConfig.java

--- src/main/java/com/multicode/payments/config/SwaggerConfig.java
+++ src/main/java/com/multicode/payments/config/SwaggerConfig.java
@@ -25,7 +25,7 @@ public class SwaggerConfig {
     public ApiInfo generateApiInfo() {
         return new ApiInfoBuilder()
                 .title("Payments API")
-                .description("API for Payments Gateway")
+                .description("API for payments gateway")
                 .build();
     }
 }
